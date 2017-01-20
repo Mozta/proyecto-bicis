84c1f2b4cf26d3210b893a1843090f5632641a88
mozta <viper_rafa_@hotmail.com>
2017-01-19 23:06:35 -0600

    Formulario dinamico

--- materialize/js/pickadate.js
+++ materialize/js/pickadate.js
@@ -7,10 +7,52 @@ $(document).ready(function() {
     $('select').material_select();
 });
 
-$('input.autocomplete').autocomplete({
-    data: {
-        "Apple": null,
-        "Microsoft": null,
-        "Google": 'http://placehold.it/250x250'
-    }
+
+$(document).ready(function() {
+    var i = 0;
+
+    $('#btAdd').click(function() {
+        if (i <= 10) {
+            i = i + 1;
+            // Añadir caja de texto.
+            $('.camposdinamicos').append('<div id=mydiv' + i + '>' +
+                '<div class="input-field col s8">' +
+                '<i class="material-icons prefix">assignment</i>' +
+                '<input id=servicio' + i + ' type="text" class="validate">' +
+                '<label for=servicio' + i + '>Servicio</label> </div>' +
+
+                '<div class="input-field col s0.5">' +
+                '<i class="fa fa-usd fa-lg" aria-hidden="true"></i></div>' +
+
+                '<div class="input-field col s2">' +
+                '<input id=precio' + i + ' type="text" class="validate">' +
+                '<label for=precio' + i + '>Precio</label>');
+
+            $('#main').after('.camposdinamicos');
+        } else { //se establece un limite para añadir elementos, 20 es el limite
+            $('.camposdinamicos').append('<label>Limite Alcanzado</label>');
+            $('#btAdd').attr('class', 'bt-disable');
+            $('#btAdd').attr('disabled', 'disabled');
+        }
+    });
+
+    $('#btRemove').click(function() { // Elimina un elemento por click
+        if (i != 0) {
+            $('#mydiv' + i).remove();
+            i = i - 1;
+        }
+
+        if (i == 0) {
+            $('.camposdinamicos').empty();
+            $('#btSubmit').remove();
+            $('#btAdd').removeAttr('disabled');
+        }
+    });
+
+    $('#btRemoveAll').click(function() { // Elimina todos los elementos del contenedor
+        $('.camposdinamicos').empty();
+        $('#btSubmit').remove();
+        i = 0;
+        $('#btAdd').removeAttr('disabled');
+    });
 });
